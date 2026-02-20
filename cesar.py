b2b5f09ba5cd00eb732a60237dfa3a87482d8339
Your Name <you@example.com>
2026-02-20 09:39:29 +0100

    yo

--- cesar.py
+++ cesar.py
@@ -6,14 +6,14 @@ meddelande = "hej"
 # nyckeln är antalet positioner varje bokstav ska flyttas
 nyckel = 1
 # variabel för att lagra det krypterade meddelandet
-resultat = ""
+
 
 # UPPDRAG: Skriv en loop som skriver ut "ifk"
 # Tips: Använd .index() och []
 
 for bokstav in meddelande:
     position = alfabet.index(bokstav)
-    ny_position = position + nyckel
-    resultat += alfabet[ny_position]
-
-print(resultat)
+    p = position + len(bokstav)
+    
+    print()
+  
